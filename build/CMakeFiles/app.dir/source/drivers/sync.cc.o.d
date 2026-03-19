8384e6210717d232ff842bdd9fc70ba1d82d8d82
chris091109 <chrisventer999@gmail.com>
2026-03-19 18:16:16 +0800

    chore: remove build dir from tracking

--- build/CMakeFiles/app.dir/source/drivers/sync.cc.o.d
+++
@@ -1,8 +0,0 @@
-CMakeFiles/app.dir/source/drivers/sync.cc.o: \
- /home/chris/fall_engine/source/drivers/sync.cc \
- /nix/store/j8irrc0mpx029dw0rmadsjylg7h31ync-glibc-2.42-51-dev/include/stdc-predef.h \
- /home/chris/fall_engine/source/drivers/sync.h \
- /home/chris/fall_engine/source/drivers/../common.h \
- /nix/store/sca0pf46jmxva40qahkcwys5c1lvk6n2-gcc-15.2.0/include/c++/15.2.0/cstdint \
- /nix/store/sca0pf46jmxva40qahkcwys5c1lvk6n2-gcc-15.2.0/include/c++/15.2.0/vector \
- /nix/store/sca0pf46jmxva40qahkcwys5c1lvk6n2-gcc-15.2.0/include/c++/15.2.0/stdexcept
